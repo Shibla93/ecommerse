2aef3f6f6852f317990f7dbd075dced9af6c16f2
Fathimath Shibla <shibisabeer@gmail.com>
2026-05-13 20:07:04 +0530

    refactor: enhance error handling and update error page layout

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,27 @@
 import Messages from '../constants/messages.js';
 import StatusCodes from '../constants/StatusCodes.js';
+// export default (err, req, res, next) => {
+//   const view = req.originalUrl.startsWith("/admin")
+//     ? "admin/error"
+//     : "user/error";
+
+//   res.status(StatusCodes.INTERNAL_SERVER_ERROR).render(view, {
+//     message: err.message || Messages.INTERNAL_SERVER_ERROR
+//   });
+// };
+
 export default (err, req, res, next) => {
+
+  console.error(err);
+
   const view = req.originalUrl.startsWith("/admin")
     ? "admin/error"
     : "user/error";
 
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).render(view, {
-    message: err.message || Messages.INTERNAL_SERVER_ERROR
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).render(view, {
+    statusCode,
+    message: err.message || "Internal Server Error"
   });
 };
